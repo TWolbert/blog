b22d7cdbf42883497e7bba964cd70d845df74827
Teun Wolbert@ubuntu <teunwolbert@outlook.com>
2024-04-15 00:05:48 +0200

    Fix

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,8 +1,6 @@
 import handleRouting from "@/server/Framework/routing";
 import { NextRequest, NextResponse } from "next/server";
 
-export const revalidate = 60 * 60 * 24; // 24 hours
-
 async function handler(req: NextRequest): Promise<NextResponse> {
     return await handleRouting(req);
 }
